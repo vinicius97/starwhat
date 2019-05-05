5978d583e0a29cfd13a4b17acbf28a3f4072eb22
Vinicius Lana <contato@viniciuslana.com>
2019-05-04 21:16:07 -0300

    Melhorar aparência dos botões de navegação

--- src/components/Pagination/PaginationNavigation.js
+++ src/components/Pagination/PaginationNavigation.js
@@ -1,14 +1,21 @@
 import React from 'react'
 
+//Assets
+import './assets/styles/PaginationNavigation.scss'
+
 export const PaginationNavigation = (props) => {
   const  { onPreviousPage, onNextPage } = props
+  const classNameMap = {
+    base: 'pagination-navigation',
+    button: 'pagination-navigation__button'
+  }
 
   return (
-    <div>
-      <button onClick={onPreviousPage}>
+    <div className={classNameMap.base}>
+      <button className={classNameMap.button} onClick={onPreviousPage}>
         Página Anterior
       </button>
-      <button onClick={onNextPage}>
+      <button className={classNameMap.button} onClick={onNextPage}>
         Proxima página
       </button>
     </div>
